f553e62ac045be47c37cf9c5f03fd4705691bd89
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-05-21 02:22:31 +0600

    global Error Handler work not complate

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,17 @@ import express, {
   type Request,
   type Response,
 } from "express";
+import { logger } from "./middleware/logger";
+import globalErrorHandler from "./middleware/globalErrorHandler";
+
 
 const app: Application = express();
+
+app.use(logger);
+
 app.get("/", (req: Request, res: Response) => {
+    throw new Error("Test error handling");
   res.send("Hello World!");
 });
-
-
+app.use(globalErrorHandler);
 export default app;
